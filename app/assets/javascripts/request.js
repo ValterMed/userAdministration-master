d115ad686e92ee1f996b1014c262607f1c1d6566
Walter Medina <waltermedinapuy@live.com>
2019-09-20 08:21:01 -0500

    modify

---
+++ app/assets/javascripts/request.js
@@ -0,0 +1,15 @@
+
+function searchCoincidence () {
+  event.preventDefault();
+  let word = $(".search").val()
+  $.ajax({
+    url: `http://localhost:3000/users/search?value=${word}`,
+    method: 'GET',
+    success: function(response) {
+      console.log(response);
+    },
+    error: function() {
+      console.log("Informacion no disponible");
+    }
+  });
+}
